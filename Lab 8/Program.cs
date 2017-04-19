3ba43f9ab4d761c4b86656d3335df382f978c994
BrettMerri <the.b.merri@gmail.com>
2017-04-19 09:31:39 -0400

    Rounds results to three decimal places. Added documentation

--- Lab 8/Program.cs
+++ Lab 8/Program.cs
@@ -10,34 +10,37 @@ namespace Lab_8
     {
         static void Main(string[] args)
         {
-            bool loop = true;
+            bool loop = true; //Declare loop to true
 
-            Console.Title = "Batting Average Calculator";
+            //Set default foreground and background color in the console.
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.Title = "Batting Average Calculator"; //Set title of the console window
             Console.WriteLine("Welcome to Batting Average Calculator!");
 
-            while (loop)
+            while (loop) //Loop main while loop is true
             {
-                Console.Write("Enter number of times at bat: ");
-                int[] TimesBatting = new int[GetValidInteger(1, 25)];
-                GetBattingResults(TimesBatting);
-                if (!ContinueApp())
+                Console.Write("Enter number of times at bat (Enter a number between 1 and 25): ");
+                int TimesBatting =  GetValidInteger(1, 25); //Gets a valid integer between 1 and 25 and assign it to TimesBatting.
+                GetBattingResults(TimesBatting); //Prompts user for batting results then calculates and prints the results.
+                if (!ContinueApp()) //Prompts user if he wants to continue. Set loop to false if use does not want to continue.
                     loop = false;
             }
         }
-
         public static int GetValidInteger(int Min, int Max)
         {
             int IntegerInput;
-            while (true)
+            while (true) //Loop GetValidInteger in case of error
             {
                 string input = Console.ReadLine();
                 if (!int.TryParse(input, out IntegerInput)) //Validates if user input is an integer
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Error: Input not a number."); //Prompts error if input is not a number
+                    Console.WriteLine("Error: Input not a number."); //Prompts error if input is not an integer
                     Console.ForegroundColor = ConsoleColor.Gray;
                     Console.Write("Try again: ");
-                    continue;
+                    continue; //Restart the loop
                 }
                 if (IntegerInput < Min || IntegerInput > Max) //Validates if user input is between Min and Max
                 {
@@ -45,37 +48,49 @@ namespace Lab_8
                     Console.WriteLine($"Error: Input not between {Min} and {Max}."); //Prompts error if input is not between Min and Max
                     Console.ForegroundColor = ConsoleColor.Gray;
                     Console.Write("Try again: ");
-                    continue;
+                    continue; //Restart the loop
                 }
                 return IntegerInput;
             }
         }
-        public static void GetBattingResults(int[] TimesBatting)
+        public static void GetBattingResults(int TimesBatting)
         {
-            int[] BattingResults = new int[TimesBatting.Length];
-            int AtBatsNotZero = 0;
-            int sum = 0;
-            for (int i = 0; i < TimesBatting.Length; i++)
+            int[] BattingResults = new int[TimesBatting]; //Create a BattingResults array of length TimesBatting
+
+            int AtBatsNotZero = 0; //Initialize int AtBatsNotZero to zero.
+            int Sum = 0; //Initialize int Sum to zero.
+
+            Console.WriteLine("0=out, 1=single, 2=double, 3=triple, 4=home run\n");
+
+            for (int i = 0; i < TimesBatting; i++)
             {
                 Console.Write($"Result for at-bat {i + 1}: ");
-                BattingResults[i] = GetValidInteger(0, 4);
-                if (BattingResults[i] != 0)
+
+                BattingResults[i] = GetValidInteger(0, 4); //Gets a valid integer between 0 and 4 and assigns it to BattingResults[i]
+
+                if (BattingResults[i] != 0) //If user input is zero, increment AtBatsNotZero
                 {
-                    AtBatsNotZero++;
+                    AtBatsNotZero++; 
                 }
-                sum += BattingResults[i];
+
+                Sum += BattingResults[i]; //Add BattingResults[i] value to Sum to get the sum of all batting results.
             }
-            double BattingAverage = (double)AtBatsNotZero / (double)BattingResults.Length;
-            double SluggingPercent = (double)sum / (double)BattingResults.Length;
+
+            //Cast double to AtBatsNotZero and Sum so the result is demimal numbers.
+            //Math.Round numbers to three decimal places
+            double BattingAverage = Math.Round(((double)AtBatsNotZero / BattingResults.Length), 3);
+            double SluggingPercent = Math.Round(((double)Sum / BattingResults.Length), 3);
+
+
             Console.WriteLine($"Batting Average: {BattingAverage}");
             Console.WriteLine($"Slugging Percentage: {SluggingPercent}");
         }
         public static bool ContinueApp()
         {
-            while (true)
+            while (true) //Loop ContinueApp in case of an error
             {
                 Console.Write("Another batter? (y/n): ");
-                string input = Console.ReadLine().ToLower();
+                string input = Console.ReadLine().ToLower(); 
                 if (input == "y")
                 {
                     Console.WriteLine();
@@ -83,7 +98,7 @@ namespace Lab_8
                 }
                 else if (input == "n")
                 {
-                    Console.WriteLine("\nByebye!");
+                    Console.WriteLine("Byebye!");
                     return false;
                 }
                 else
@@ -94,7 +109,6 @@ namespace Lab_8
                 }
             }
         }
-
     }
 }
 //SP = SUM/Number of Atbats
